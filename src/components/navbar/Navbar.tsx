f487ccceed7746645b0fa3a38bcc5ea3a5a84b35
Andrey Malin <andreu6454@gmail.com>
2025-04-07 17:18:34 +0300

    feat(navbar): add navBarItems to reusable constant

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,39 +3,11 @@ import { Image, Text } from '@chakra-ui/react';
 import { memo } from 'react';
 
 import { NavbarItem } from '~/components/navbar/NavbarItem/NavbarItem';
+import { navBarItems } from '~/shared/data/nav-items';
 
-import ChildrensIcon from '../../assets/svg/childrens.svg';
-import DessertsIcon from '../../assets/svg/desserts.svg';
-import DrinksIcon from '../../assets/svg/drinks.svg';
-import FirstCoursesIcon from '../../assets/svg/firstCourses.svg';
-import GrilledIcon from '../../assets/svg/grilled.svg';
 import LeaveIcon from '../../assets/svg/leaveIcon.svg';
-import NationalIcon from '../../assets/svg/national.svg';
-import PreparationsIcon from '../../assets/svg/preparations.svg';
-import SaladsIcon from '../../assets/svg/salads.svg';
-import SaucesIcon from '../../assets/svg/sauces.svg';
-import SecondCoursesIcon from '../../assets/svg/secondCourses.svg';
-import SnacksIcon from '../../assets/svg/snacks.svg';
-import TherapeuticNutritionIcon from '../../assets/svg/therapeuticNutrition.svg';
-import VeganCuisineIcon from '../../assets/svg/veganCuisine.svg';
 
 export const Navbar = memo(() => {
-    const navBarItems = [
-        { icon: SaladsIcon, title: 'Салаты' },
-        { icon: SnacksIcon, title: 'Закуски' },
-        { icon: FirstCoursesIcon, title: 'Первые блюда' },
-        { icon: SecondCoursesIcon, title: 'Вторые блюда' },
-        { icon: DessertsIcon, title: 'Десерты, выпечка' },
-        { icon: GrilledIcon, title: 'Блюда на гриле' },
-        { icon: VeganCuisineIcon, title: 'Веганская кухня' },
-        { icon: ChildrensIcon, title: 'Детские блюда' },
-        { icon: TherapeuticNutritionIcon, title: 'Лечебное питание' },
-        { icon: NationalIcon, title: 'Национальные' },
-        { icon: SaucesIcon, title: 'Соусы' },
-        { icon: DrinksIcon, title: 'Напитки' },
-        { icon: PreparationsIcon, title: 'Заготовки' },
-    ];
-
     const mappedItems = navBarItems.map((el) => (
         <NavbarItem key={el.title} title={el.title} icon={el.icon} />
     ));
---
+++ src/shared/data/nav-items.ts
@@ -0,0 +1,29 @@
+import ChildrensIcon from '~/assets/svg/childrens.svg';
+import DessertsIcon from '~/assets/svg/desserts.svg';
+import DrinksIcon from '~/assets/svg/drinks.svg';
+import FirstCoursesIcon from '~/assets/svg/firstCourses.svg';
+import GrilledIcon from '~/assets/svg/grilled.svg';
+import NationalIcon from '~/assets/svg/national.svg';
+import PreparationsIcon from '~/assets/svg/preparations.svg';
+import SaladsIcon from '~/assets/svg/salads.svg';
+import SaucesIcon from '~/assets/svg/sauces.svg';
+import SecondCoursesIcon from '~/assets/svg/secondCourses.svg';
+import SnacksIcon from '~/assets/svg/snacks.svg';
+import TherapeuticNutritionIcon from '~/assets/svg/therapeuticNutrition.svg';
+import VeganCuisineIcon from '~/assets/svg/veganCuisine.svg';
+
+export const navBarItems = [
+    { icon: SaladsIcon, title: 'Салаты' },
+    { icon: SnacksIcon, title: 'Закуски' },
+    { icon: FirstCoursesIcon, title: 'Первые блюда' },
+    { icon: SecondCoursesIcon, title: 'Вторые блюда' },
+    { icon: DessertsIcon, title: 'Десерты, выпечка' },
+    { icon: GrilledIcon, title: 'Блюда на гриле' },
+    { icon: VeganCuisineIcon, title: 'Веганская кухня' },
+    { icon: ChildrensIcon, title: 'Детские блюда' },
+    { icon: TherapeuticNutritionIcon, title: 'Лечебное питание' },
+    { icon: NationalIcon, title: 'Национальные' },
+    { icon: SaucesIcon, title: 'Соусы' },
+    { icon: DrinksIcon, title: 'Напитки' },
+    { icon: PreparationsIcon, title: 'Заготовки' },
+];
