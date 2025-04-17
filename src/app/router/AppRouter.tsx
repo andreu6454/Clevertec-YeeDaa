87bf345524a79add277040923302b20e02ccf9a6
Andrey Malin <andreu6454@gmail.com>
2025-04-17 19:46:28 +0300

    feat(*): start sprint_2
    -add translation to all routes in BreadCrumbs

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,19 +1,16 @@
-import { Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router';
 
 import { JuiciestFood } from '~/pages/JuiciestFood/JuiciestFood';
 import { MainPage } from '~/pages/MainPage/MainPage.tsx';
-import { VeganPage } from '~/pages/VeganPage/VeganPage';
+import { NavBarData } from '~/shared/data/navBarData';
+
+import { createRoutes } from './routeComponents';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>sdf</div>}>
-        <Routes>
-            <Route index element={<MainPage />} />
-            <Route path='/vegan'>
-                <Route index element={<Navigate to='second-courses' replace />} />
-                <Route path=':id' element={<VeganPage />} />
-            </Route>
-            <Route path='/juiciest' element={<JuiciestFood />} />
-        </Routes>
-    </Suspense>
+    <Routes>
+        <Route index element={<MainPage />} />
+        {createRoutes({ data: NavBarData })}
+        <Route path='/juiciest' element={<JuiciestFood />} />
+        {/*<Route path='*' element={<div>404</div>}/>*/}
+    </Routes>
 );
