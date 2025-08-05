79c48f522397b9b1ffa0348c18a57193207eeba8
Andrey Malin <andreu6454@gmail.com>
2025-08-05 18:15:05 +0300

    refactor(*): change preview port

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(() => ({
         host: true,
         port: 3000,
     },
+    preview: {
+        port: 3000,
+    },
     resolve: {
         alias: {
             '~': resolve(__dirname, 'src'),
